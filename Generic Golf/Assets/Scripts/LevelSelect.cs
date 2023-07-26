f5b73d4394ebf760a8da9b813ca10e479042de82
aunashraf12 <aun241006@gmail.com>
2023-07-26 12:00:21 +0500

    level select

---
+++ Generic Golf/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OpenScene(){
+
+        Debug.Log("loaded scene");
+
+    }
+}
