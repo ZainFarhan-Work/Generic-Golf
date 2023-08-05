b0246a8f2d15f0a1e923ae1b41c4d454ee6f68bc
ZainFarhan-Work <zainfarhan.work@gmail.com>
2023-08-05 23:50:14 +0500

    Score 1

--- Generic Golf/Assets/Scripts/Physics/PlayerControls.cs
+++ Generic Golf/Assets/Scripts/Physics/PlayerControls.cs
@@ -9,15 +9,14 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    [SerializeField] private int maxShots;
-    [Space]
+    
     [SerializeField] private int shotPower;
     [SerializeField] private float maxSpeed;
     [Space]
     [SerializeField] private float frictionCoeff;
     [SerializeField] private float stopVelocity;
     [NonSerialized] public Vector3 origin;
-    private TextMeshProUGUI strokes { get; set; }
+    
     private GameObject pauseMenu { get; set; }
     private GameObject gameOver { get; set; }
     private LineRenderer line { get; set; }
@@ -27,10 +26,40 @@ public class PlayerControls : MonoBehaviour
     private bool isIdle { get; set; }
     private bool isAiming { get; set; }
 
+    // Score Variables
+
+    [Space]
+    [Header("Score")]
+    [SerializeField] private int maxShots;
+    private GameObject scoreBoard { get; set; }
+    private TextMeshProUGUI strokes { get; set; }
+    private TextMeshProUGUI hits { get; set; }
+    private TextMeshProUGUI scoreText { get; set; }
+    private TextMeshProUGUI grade { get; set; }
+    private float remainingShots { get; set; }
+    private int strokesHit { get; set; }
+    private float score { get; set; }
+    private float shotPercentage { get; set; }
+    
+
     private void Awake()
     {
+        // Starting Score
+
+        scoreBoard = GameObject.FindGameObjectWithTag("ScoreBoard");
+
+        hits = GameObject.Find("Hits").GetComponent<TextMeshProUGUI>();
+        scoreText = GameObject.Find("Score").GetComponent<TextMeshProUGUI>();
+        grade = GameObject.Find("Grade").GetComponent<TextMeshProUGUI>();
+
+        scoreBoard.SetActive(false);
+
+
+        remainingShots = maxShots;
         strokes = GameObject.Find("Stroke Count").GetComponent<TextMeshProUGUI>();
-        strokes.text = "Strokes:" + maxShots;
+        strokes.text = "Strokes:" + remainingShots;
+
+        // End of Score
 
         gameOver = GameObject.FindGameObjectWithTag("Game Over");
         gameOver.SetActive(false);
@@ -56,6 +85,8 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Score();
+
         ProcessAiming();
 
         if (playerBody.velocity.magnitude < stopVelocity)
@@ -76,6 +107,17 @@ public class PlayerControls : MonoBehaviour
         {
             Pause();
         }
+
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            scoreBoard.SetActive(true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            scoreBoard.SetActive(false);
+        }
+
 
     }
 
@@ -143,9 +185,10 @@ public class PlayerControls : MonoBehaviour
         isAiming = false;
         isIdle = false;
         line.enabled = false;
-        maxShots--;
+        strokesHit++;
+        remainingShots--;
 
-        strokes.text = "Strokes:" + maxShots;
+        strokes.text = "Strokes:" + remainingShots;
 
         Vector2 localDirection = direction - transform.position;
 
@@ -195,6 +238,45 @@ public class PlayerControls : MonoBehaviour
            gameOver.SetActive(true);
     }
 
+    private void Score()
+    {
+
+        hits.text = "Hits: " + strokesHit + "/" + maxShots;
+
+        if (strokesHit > 0)
+        {
+            score = Mathf.RoundToInt((1.0f / strokesHit) * 1000);
+        }        
+
+        scoreText.text = "Score: " + score;
+
+        shotPercentage = Mathf.RoundToInt(remainingShots / maxShots * 100.0f);
+
+        if (shotPercentage >= 90)
+        {
+            grade.text = "A+";
+        }
+        else if (shotPercentage >= 80)
+        {
+            grade.text = "A";
+        }
+        else if (shotPercentage >= 70)
+        {
+            grade.text = "B";
+        }
+        else if (shotPercentage >= 60)
+        {
+            grade.text = "C";
+        }
+        else if (shotPercentage >= 50)
+        {
+            grade.text = "D";
+        }
+        else
+        {
+            grade.text = "F";
+        }
+    }
 
 
 
